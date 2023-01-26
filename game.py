70a8718f46bf3cce5e429a2dfd501932161f3e0b
tim a <timfosho@gmail.com>
2023-01-25 20:51:58 -0500

    added classes

--- game.py
+++ game.py
@@ -1,65 +1,80 @@
-# GAME 
-
 import pygame 
 import random 
 
-imgnumint = 1
-
-def randc():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    return (r,g,b)
-
-def imgnum(imgnumint):
-    imgnumint += 1
-    return imgnumint
-
-
-WIDTH = 1900
-HEIGHT = 1000
+run = True
+WIDTH = 1200
+HEIGHT = 700
 WHITE = (155,155,255)
 BLACK = (0,0,0)
+VEL = 5
 
 pygame.init()   
-
 window = pygame.display.set_mode((WIDTH, HEIGHT)) 
-pygame.display.set_caption("Drawing Design") 
+pygame.display.set_caption("My Game") 
 
-x = WIDTH
-y = HEIGHT
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-width = 5
-height = 5
-vel = 5
-run = True
+    def update(self):
+        pygame.draw.rect(window, WHITE, (self.x, self.y, self.width, self.height)) 
 
+    def input(self, keys):
+        if keys[pygame.K_LEFT] and self.x > 0: 
+            self.left()
+        if keys[pygame.K_RIGHT] and self.x < WIDTH: 
+            self.right()
+        if keys[pygame.K_UP] and self.y > 0: 
+            self.up()
+        if keys[pygame.K_DOWN] and self.y < HEIGHT: 
+            self.down()
 
+    def up(self):
+        self.y -= VEL
+        
+    def down(self):
+        self.y += VEL
+    
+    def left(self):
+        self.x -= VEL
+    
+    def right(self):
+        self.x += VEL
 
+class Ball(Pos):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.width = 10
+        self.height = 10
 
+ball = Ball(50, 50)
+ball2 = Ball(100, 100)
+ball2.width = 5
 
+######################################################
+## MAIN LOOP
+######################################################
 while run:  
     pygame.time.delay(10)  
     
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             run = False
+
     keys = pygame.key.get_pressed() 
-    
-    if keys[pygame.K_LEFT] and x > 0: 
-        x -= vel 
-    if keys[pygame.K_RIGHT] and x < WIDTH - width: 
-        x += vel 
-    if keys[pygame.K_UP] and y > 0: 
-        y -= vel 
-    if keys[pygame.K_DOWN] and y < HEIGHT - height: 
-        y += vel 
     if keys[pygame.K_RSHIFT]: 
         window.fill(BLACK)       
-    if keys[pygame.K_LSHIFT]: 
-        window.fill(randc()) 
-    if keys[pygame.K_TAB]: 
-        pygame.image.save(window, "screenshot_%s.jpeg" % (imgnum(imgnumint) - 1))        
-    pygame.draw.rect(window, WHITE, (x, y, width, height)) 
+    if keys[pygame.K_ESCAPE]: 
+        run = False       
+
+    ball.input(keys)
+    ball.update()
+
+    ball2.input(keys)
+    ball2.update()
+
+
     pygame.display.update() 
+
 pygame.quit()  
